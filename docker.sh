5223e4aefe48ec8164749f1da2e8cdacd7f68042
1015156849 <1015156849@qq.com>
2020-08-20 12:21:21 +0800

    add sh

---
+++ docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "docker管理脚本"
+echo "【1】启动nginx并挂载vue-pcr dist目录"
+echo
+echo
+echo "【其他任意字符退出】"
+echo "---------------------请输入序号------------------------"
+read code
+
+if [[ $code -eq 1 ]]
+then
+	echo "开始执行nginx部署命令。。。"
+else
+	echo "退出脚本"
+fi
