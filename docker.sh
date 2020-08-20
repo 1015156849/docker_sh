0c0665bf5eea51ba26638a4c11e13e4ee209279f
1015156849 <1015156849@qq.com>
2020-08-20 12:51:12 +0800

    no message

--- docker.sh
+++ docker.sh
@@ -7,19 +7,19 @@ YELLOW='\e[1;33m' # 黄
 BLUE='\e[1;34m' # 蓝
 PINK='\e[1;35m' # 粉红
 RES='\e[0m' # 清除颜色
-
-echo -e "${GREEN} docker管理脚本 ${GREEN}"
-echo -e "${GREEN} 【1】启动nginx并挂载vue-pcr dist目录 ${GREEN}"
+# 开始脚本
+echo -e "${GREEN}------------------docker管理脚本----------------------${RES}"
+echo -e "${GREEN}----【1】启动nginx并挂载vue-pcr dist目录 ${RES}"
 echo
 echo
-echo -e "${GREEN}【其他任意字符退出】${GREEN}"
-echo -e "${GREEN}---------------------请输入序号------------------------${GREEN}"
+echo -e "${GREEN}----【其他任意字符退出】${RES}"
+echo -e "${GREEN}---------------------请输入序号------------------------${RES}"
 read code
 
 if [[ $code -eq 1 ]]
 then
-	echo -e "${GREEN}开始执行nginx部署命令。。。${GREEN}"
+	echo -e "${GREEN}->>开始执行nginx部署命令。。。${RES}"
 else
-	echo -e "${GREEN}退出脚本${GREEN}"
+	echo -e "${GREEN}退出脚本${RES}"
 fi
-echo -e "${RES}${RES}"
+echo -e "${BLUE}退出${RES}"
