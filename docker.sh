d559d1395e5829fc35ddc91b1be999a4aa05042c
1015156849 <1015156849@qq.com>
2020-08-20 13:42:24 +0800

    no message

--- docker.sh
+++ docker.sh
@@ -9,14 +9,38 @@ PINK='\e[1;35m' # 粉红
 RES='\e[0m' # 清除颜色
 # 开始脚本
 echo -e "${GREEN} docker管理脚本--------------- ${RES}"
-echo -e "${GREEN} 【1】启动nginx并挂载vue-pcr dist目录${RES}"
-echo -e "${GREEN} 【其他任意字符退出】 ${RES}"
+echo -e "${GREEN} 【1】安装nginx镜像并创建宿主机配置文件目录和配置文件${RES}"
+echo -e "${GREEN} 【2】重启nginx${RES}"
+echo -e "${GREEN} 【3】启动nginx并挂载vue-pcr dist目录${RES}"
+echo -e "${GREEN} 【3】退出 ${RES}"
+echo -e "${GREEN} 【0】退出 ${RES}"
 echo -e "${GREEN} 请输入序号----------- ${RES}"
 read code
-if [[ $code -eq 1 ]]
-then
-	echo -e "${GREEN}->>开始执行nginx部署命令。。。${RES}"
-else
+case "$code" in
+1)
+    echo -e "${YELLOW}->>开始执行nginx部署命令。。。${RES}"
+    {
+        docker stop nginx-test
+        mkdir -p ~/data/nginx/{conf,conf.d,html,log}
+        cp ~/docker_sh/nginx/nginx.conf ~/data/nginx/conf/
+        docker run --name nginx-test -v ~/data/nginx/conf/nginx.conf:/etc/nginx/nginx.conf -v ~/data/nginx/log:/var/log/nginx -v ~/data/nginx/html:/usr/share/nginx/html nginx
+    }
+;;
+2)
+    echo -e "${YELLOW}->>开始重启nginx。。。${RES}"
+    source docker restart nginx-test
+    echo -e "${BLUE}重启nginx完成${RES}"
+;;
+3)
+	echo -e "${YELLOW}->>开始执行nginx部署命令。。。${RES}"
+    clear
+    echo -e "${YELLOW}关闭已启动的vue-pcr镜像${RES}"
+    source docker stop vuepcr
+    echo -e "${YELLOW}启动vue-pcr镜像${RES}"
+    source docker run -itd --name vuepcr vuepcr:1.0 
+    echo -e "${YELLOW}启动nginx镜像并挂在vuepcr${RES}"
+;;
+0)
 	echo -e "${GREEN}退出脚本${RES}"
+    exit;
 fi
-echo -e "${BLUE}退出${RES}"
