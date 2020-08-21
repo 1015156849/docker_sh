bd9a7b18cfd2ff2551e848081680dea214545db9
1015156849 <1015156849@qq.com>
2020-08-21 15:04:46 +0800

    no message

--- docker.sh
+++ docker.sh
@@ -9,57 +9,24 @@ PINK='\e[1;35m' # 粉红
 RES='\e[0m' # 清除颜色
 # 开始脚本
 echo -e "${GREEN} docker管理脚本--------------- ${RES}"
-echo -e "${GREEN} 【1】安装nginx镜像并创建宿主机配置文件目录和配置文件【自动导入pcr-guild-vue】${RES}"
-echo -e "${GREEN} 【2】重启nginx${RES}"
-echo -e "${GREEN} 【3】启动pcr-guild 后台${RES}"
-echo -e "${GREEN} 【0】退出 ${RES}"
+echo -e "${GREEN} 【1】部署pcr_box网站前台${RES}"
+echo -e "${GREEN} 【2】部署pcr_box网站后台到docker并启动${RES}"
 echo -e "${GREEN} 请输入序号----------- ${RES}"
 read code
 case "$code" in
 1)
-    echo -e "${YELLOW}->>开始执行nginx部署命令。。。${RES}"
+    echo -e "${YELLOW}->>开始执行 部署pcr_box网站前台 命令${RES}"
     {
-        mkdir -p ~/data/nginx/{conf,conf.d,html,log}
-        cp ~/docker_sh/nginx/nginx.conf ~/data/nginx/conf/
-        cp ~/docker_sh/nginx/index.html ~/data/nginx/html
-        cp ~/docker_sh/nginx/50x.html ~/data/nginx/html
-        mkdir -p ~/data/nginx/html/vuepcr
-        cd ~/data/nginx/html/vuepcr 
-        wget -c https://github.com/CJowo/pcr-guild-vue/releases/download/0.2.1/dist.zip
-        unzip dist.zip
-        rm dist.zip
-        docker run -d -p 8080:80 --name nginx -v ~/data/nginx/conf/nginx.conf:/etc/nginx/nginx.conf -v ~/data/nginx/log:/var/log/nginx -v ~/data/nginx/html:/usr/share/nginx/html nginx
+        ./vue-pcr/vue-pcr.sh
     }
-;;
-2)
-    echo -e "${YELLOW}->>开始重启nginx。。。${RES}"
-    {
-        cp ~/docker_sh/nginx/nginx.conf ~/data/nginx/conf/
-        docker restart nginx
-    }
-    echo -e "${BLUE}重启nginx完成${RES}"
 ;;
 3)
-    echo -e "${YELLOW}->>开始执行pcr-guild 后台服务 命令。。。${RES}"
-    clear
-    echo -e "${YELLOW}启动pcr-guild 后台服务镜像${RES}"
+    echo -e "${YELLOW}->>开始执行 部署pcr_box网站后台到docker并启动 命令${RES}"
     {
-        docker run -itd -p 8081:80 --name vuepcr-server vuepcrserver:1.0 
+        ./vue-pcr-server/vue-pcr-server.sh
     }
 ;;
-5)
-	echo -e "${YELLOW}->>开始执行nginx部署命令。。。${RES}"
-    clear
-    echo -e "${YELLOW}启动vue-pcr镜像${RES}"
-    {
-        docker run -itd --name vuepcr -v ~/data/nginx/html/vuepcr:/data/pcr-guild-vue/dist vuepcr:1.0 
-    } 
-    echo -e "${YELLOW}启动nginx镜像并挂在vuepcr${RES}"
-;;
-0)
-	echo -e "${GREEN}退出脚本${RES}"
-    exit;
-;;
 *)
+echo -e "${GREEN}退出脚本${RES}"
 ;;
 esac
---
+++ vue-pcr/vue-pcr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+# 定义颜色变量
+RED='\e[1;31m' # 红
+GREEN='\e[1;32m' # 绿
+YELLOW='\e[1;33m' # 黄
+BLUE='\e[1;34m' # 蓝
+PINK='\e[1;35m' # 粉红
+RES='\e[0m' # 清除颜色
+# 开始脚本
+echo -e "${YELLOW}->>开始执行 部署pcr_box网站前台脚本${RES}"
+{
+        mkdir -p /var/www/html/vuepcr
+        cd /var/www/html/vuepcr 
+        wget -c https://github.com/CJowo/pcr-guild-vue/releases/download/0.2.1/dist.zip
+        unzip dist.zip
+        rm dist.zip
+        cp ~/docker_sh/vue-pcr/caddy_vue_pcr_box /etc/caddy/sites
+        service caddy reload
+}
+echo -e "${GREEN}->>部署完毕${RES}"
