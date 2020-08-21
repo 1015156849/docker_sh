cb1404829ac9aeb358c55e9e04aeeaa499db6d08
1015156849 <1015156849@qq.com>
2020-08-21 17:47:54 +0800

    no message

--- vue-pcr/vue-pcr.sh
+++ vue-pcr/vue-pcr.sh
@@ -16,7 +16,7 @@ echo -e "${YELLOW}->>开始执行 部署pcr_box网站前台脚本${RES}"
         wget -c https://github.com/CJowo/pcr-guild-vue/releases/download/0.2.1/dist.zip
         unzip dist.zip
         rm dist.zip
-        cp ~/docker_sh/vue-pcr/caddy_vue_pcr_box /etc/caddy/sites
-        service caddy reload
+        # cp ~/docker_sh/vue-pcr/caddy_vue_pcr_box /etc/caddy/sites
+        # service caddy reload
 }
 echo -e "${GREEN}->>部署完毕${RES}"
