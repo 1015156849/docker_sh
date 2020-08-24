09fd36d320530693c8f90e9c62281a2084939ea5
1015156849 <1015156849@qq.com>
2020-08-24 16:31:01 +0800

    修改docker镜像生成脚本

--- docker.sh
+++ docker.sh
@@ -11,8 +11,9 @@ RES='\e[0m' # 清除颜色
 echo -e "${GREEN} docker管理脚本--------------- ${RES}"
 echo -e "${GREEN} 【1】注册dockerManager${RES}"
 echo -e "${GREEN} 【2】部署pcr_box网站前台${RES}"
-echo -e "${GREEN} 【3】部署pcr_box网站后台到docker并启动${RES}"
-echo -e "${GREEN} 【4】部署pcr_box caddy ${RES}"
+echo -e "${GREEN} 【3】重新Build pcr_box网站后台 Docker镜像${RES}"
+echo -e "${GREEN} 【4】部署pcr_box网站后台镜像到docker并启动${RES}"
+echo -e "${GREEN} 【5】更新pcr_box caddy ${RES}"
 echo -e "${GREEN} 请输入序号----------- ${RES}"
 read code
 case "$code" in
@@ -33,6 +34,15 @@ case "$code" in
     }
 ;;
 3)
+    echo -e "${YELLOW}->>开始执行 重新Build pcr_box网站后台 Docker镜像 命令${RES}"
+    {
+        docker rmi vuepcrserver
+        cd ./vue-pcr-server
+        docker build -t vuepcrserver:1.0 .
+    }
+    echo -e "${GREEN}->>执行完毕${RES}"
+;;
+4)
     echo -e "${YELLOW}->>开始执行 部署pcr_box网站后台到docker并启动 命令${RES}"
     {
        chmod +x ./vue-pcr-server/vue-pcr-server.sh
@@ -40,7 +50,7 @@ case "$code" in
        chmod -x ./vue-pcr-server/vue-pcr-server.sh
     }
 ;;
-4)
+5)
     echo -e "${YELLOW}->>开始执行 部署pcr_box caddy 命令${RES}"
     {
         cp ~/docker_sh/vue-pcr-server/caddy_vue_pcr_box_server /etc/caddy/sites
