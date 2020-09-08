d464418671af05ec63a7e7b227794278902fbe48
NightCat6849 <yangzhenyu6849@gmail.com>
2020-09-08 21:03:49 +0800

    提交修改

--- install.sh
+++ install.sh
@@ -79,35 +79,40 @@ _load() {
 	local _dir="/etc/ChaChaPRO/pcrbox/src/"
 	. "${_dir}$@"
 }
+update(){
+    $cmd update -y
+    if [[ $cmd == "apt-get" ]]; then
+    $cmd install -y lrzsz git zip unzip curl wget qrencode libcap2-bin dbus
+    else
+    # $cmd install -y lrzsz git zip unzip curl wget qrencode libcap iptables-services
+    $cmd install -y lrzsz git zip unzip curl wget qrencode libcap
+    fi
+    [ -d /etc/ChaChaPRO ] && rm -rf /etc/ChaChaPRO
+
+    mkdir -p /etc/ChaChaPRO/pcrbox
+    cp -rf $(pwd)/* /etc/ChaChaPRO/pcrbox
+    # pushd /tmp
+    # git clone https://github.com/1015156849/docker_sh -b "$_gitbranch" /etc/ChaChaPRO/pcrbox --depth=1
+    # popd
+
+    if [[ ! -d /etc/ChaChaPRO/pcrbox ]]; then
+        echo
+        echo -e "$red 哎呀呀...克隆脚本仓库出错了...$none"
+        echo
+        echo -e " 温馨提示..... 请尝试自行安装 Git: ${green}$cmd install -y git $none 之后再安装此脚本"
+        echo
+        exit 1
+    else
+        echo "脚本更新完毕"
+    fi
+}
 initInstall(){
     if [[ -d /etc/ChaChaPRO/pcrbox ]]; then
 		echo
 		echo " 已初始化该脚本"
 		echo
 	else
-        $cmd update -y
-        if [[ $cmd == "apt-get" ]]; then
-        $cmd install -y lrzsz git zip unzip curl wget qrencode libcap2-bin dbus
-        else
-        # $cmd install -y lrzsz git zip unzip curl wget qrencode libcap iptables-services
-        $cmd install -y lrzsz git zip unzip curl wget qrencode libcap
-        fi
-        [ -d /etc/ChaChaPRO ] && rm -rf /etc/ChaChaPRO
-
-        mkdir -p /etc/ChaChaPRO/pcrbox
-        cp -rf $(pwd)/* /etc/ChaChaPRO/pcrbox
-        # pushd /tmp
-        # git clone https://github.com/1015156849/docker_sh -b "$_gitbranch" /etc/ChaChaPRO/pcrbox --depth=1
-        # popd
-
-        if [[ ! -d /etc/ChaChaPRO/pcrbox ]]; then
-            echo
-            echo -e "$red 哎呀呀...克隆脚本仓库出错了...$none"
-            echo
-            echo -e " 温馨提示..... 请尝试自行安装 Git: ${green}$cmd install -y git $none 之后再安装此脚本"
-            echo
-            exit 1
-        fi
+       update
 	fi
     
 }
@@ -210,6 +215,8 @@ while :; do
 	echo
     echo -e "$yellow 5. 单独安装具体功能$none"
 	echo
+    echo -e "$yellow 6. 更新脚本$none"
+	echo
 	read -p "$(echo -e "请选择 [${magenta}1-5$none]:")" choose
 	case $choose in
 	1)
@@ -235,7 +242,11 @@ while :; do
         initInstall
         customInstall
         break
-        ;;        
+        ;;
+    6)
+        update
+        break
+        ;;          
 	*)
 		error
 		;;
