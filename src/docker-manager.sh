307aa611a6815ff5c000d35b44d7f460ebcc0fa7
1015156849 <yangzhenyu6849@foxmail.com>
2020-09-09 17:08:57 +0800

    新增Docker管理网站以及汉化文件

--- src/docker-manager.sh
+++ src/docker-manager.sh
@@ -26,7 +26,7 @@ _install_docker_manager(){
 	done
 }
 _caddy_conf(){
-    
+
 }
 _uninstall_docker_manager(){
     docker stop portainer
@@ -40,10 +40,10 @@ _menu_install_docker_manager(){
             echo
             echo -e "$yellow 安装的管理面板为 Portainer.io，因为823 pma安全事故，暂不加入国产的宝塔平台安装脚本。如果必须要用宝塔，请自行动手 $none"
             echo
-            echo -e "请选择 "$yellow"功能"$none" 序号 [${magenta}1-${#menu_list_docker[*]}$none]"
+            echo -e "请选择 "$yellow"功能"$none" 序号 [${magenta}1-${#menu_list_docker_manager[*]}$none]"
             echo
-            for ((i = 1; i <= ${#menu_list_docker[*]}; i++)); do
-                Stream="${menu_list_docker[$i - 1]}"
+            for ((i = 1; i <= ${#menu_list_docker_manager[*]}; i++)); do
+                Stream="${menu_list_docker_manager[$i - 1]}"
                 if [[ "$i" -le 9 ]]; then
                     # echo
                     echo -e "$yellow  $i. $none${Stream}"
